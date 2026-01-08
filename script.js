88410d16647b50f9282fb1f0352943558144caae
Stan Commandeur <66023078+CommandeurStan@users.noreply.github.com>
2026-01-08 08:34:35 -0500

    capatalizes last array object, yet objective is to 'Camalize'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+/* const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 
 function sumOfTripledEvens(array) {
@@ -17,4 +17,20 @@ function sumOfTripledEvens(array) {
     return;
   
 }
-console.log(sumOfTripledEvens(arr));
+console.log(sumOfTripledEvens(arr)); */
+
+function camelize(str) {
+  const array = str.split('-')
+  const capBegin = array.findLast(word => word.length - 1)
+  const capWord = capBegin[0].toUpperCase() + capBegin.slice(1)
+  return array.splice()+capWord;
+  // return capBegin;
+      
+}
+   
+
+console.log(camelize("background-color"));
+console.log(camelize("background-color") == 'backgroundColor');
+console.log(camelize("list-style-image"));
+console.log(camelize("list-style-image") == 'listStyleImage');
+// console.log(camelize("-webkit-transition") == 'WebkitTransition');
